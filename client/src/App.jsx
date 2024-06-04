1b0d6e96492532dad207ee32e22c5f61007dee2f
Thirumalaikumar-M <mthirumalaitk@gmail.com>
2024-06-04 17:49:36 +0530

    Style entire application

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,4 @@
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
-
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Write from "./pages/Write";
@@ -7,6 +6,8 @@ import Home from "./pages/Home";
 import Single from "./pages/Single";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+
+import "./style.scss";
 
 const Layout = () => {
   return (
