85a2bd76454d6a2e2f30b89ce9b47ddb11adbc5f
Thirumalaikumar-M <mthirumalaitk@gmail.com>
2024-07-28 15:34:12 +0530

    Setup server routes

--- server/index.js
+++ server/index.js
@@ -1,7 +1,12 @@
 import express from "express";
 
 const app = express();
+
 const PORT = process.env.PORT || 8800;
+
+app.use("/api/auth");
+app.use("/api/user");
+app.use("/api/post");
 
 app.listen(PORT, () => {
   console.log("Connected");
