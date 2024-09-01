d5e3e043e7d5b7cb24283acf1c6b564116a5f31c
Thirumalaikumar-M <mthirumalaitk@gmail.com>
2024-09-01 15:25:09 +0530

    Update components

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,13 +1,18 @@
-import React from "react";
-import Logo from "../assets/blog-logo.png";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../context/authContext";
+import Logo from "../assets/blog-logo.png";
 
 const Navbar = () => {
+  const { currentUser, logout } = useContext(AuthContext);
+
   return (
     <div className="navbar">
       <div className="container">
         <div className="logo">
-          <img src={Logo} alt="logo" />
+          <Link to="/">
+            <img src={Logo} alt="" />
+          </Link>
         </div>
         <div className="links">
           <Link className="link" to="/?cat=art">
@@ -22,16 +27,22 @@ const Navbar = () => {
           <Link className="link" to="/?cat=cinema">
             <h6>CINEMA</h6>
           </Link>
-          <Link className="link" to="/?cat=food">
-            <h6>FOOD</h6>
-          </Link>
           <Link className="link" to="/?cat=design">
             <h6>DESIGN</h6>
           </Link>
-          <span>Thiru</span>
-          <span>Logout</span>
+          <Link className="link" to="/?cat=food">
+            <h6>FOOD</h6>
+          </Link>
+          <span>{currentUser?.username}</span>
+          {currentUser ? (
+            <span onClick={logout}>Logout</span>
+          ) : (
+            <Link className="link" to="/login">
+              Login
+            </Link>
+          )}
           <span className="write">
-            <Link to="/write" className="link">
+            <Link className="link" to="/write">
               Write
             </Link>
           </span>
