ed3b5176363357ea1ed406135fe4566040a25dff
Thirumalaikumar-M <mthirumalaitk@gmail.com>
2024-09-01 16:22:49 +0530

    Fix main function err

--- server/index.js
+++ server/index.js
@@ -1,13 +1,34 @@
 import express from "express";
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
+import cookieParser from "cookie-parser";
+import multer from "multer";
 
 const app = express();
 
-const PORT = process.env.PORT || 8800;
+app.use(express.json());
+app.use(cookieParser());
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "../client/public/upload");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + file.originalname);
+  },
+});
 
-app.use("/api/auth");
-app.use("/api/user");
-app.use("/api/post");
+const upload = multer({ storage });
 
-app.listen(PORT, () => {
-  console.log("Connected");
+app.post("/api/upload", upload.single("file"), function (req, res) {
+  const file = req.file;
+  res.status(200).json(file.filename);
+});
+
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+
+app.listen(8800, () => {
+  console.log("Connected!");
 });
